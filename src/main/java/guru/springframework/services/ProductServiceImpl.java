de4371b8c704eea9b2258b648e9fd2bb01820a1d
Jakub Vrana <jvrana@monetplus.cz>
2019-10-22 21:00:45 +0200

    Added custom metrics counter - using gauge service GaugeService

--- src/main/java/guru/springframework/services/ProductServiceImpl.java
+++ src/main/java/guru/springframework/services/ProductServiceImpl.java
@@ -6,6 +6,7 @@ import guru.springframework.services.jms.JmsTextMessageService;
 import org.apache.commons.collections4.IteratorUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.metrics.CounterService;
+import org.springframework.boot.actuate.metrics.GaugeService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,27 +17,36 @@ import java.util.List;
 @Service
 public class ProductServiceImpl implements ProductService {
 
-    private ProductRepository productRepository;
-    private JmsTextMessageService jmsTextMessageService;
-    private CounterService counterService;
+    private final ProductRepository productRepository;
+    private final JmsTextMessageService jmsTextMessageService;
+    private final CounterService counterService;
+    private final GaugeService gaugeService;
 
     @Autowired
-    public ProductServiceImpl(ProductRepository productRepository, JmsTextMessageService jmsTextMessageService, CounterService counterService) {
+    public ProductServiceImpl(ProductRepository productRepository,
+                              JmsTextMessageService jmsTextMessageService,
+                              CounterService counterService,
+                              GaugeService gaugeService) {
         this.productRepository = productRepository;
         this.jmsTextMessageService = jmsTextMessageService;
         this.counterService = counterService;
+        this.gaugeService = gaugeService;
     }
 
     @Override
     public Product getProduct(Integer id) {
         jmsTextMessageService.sendTextMessage("Fetching Product ID: " + id );
-        counterService.increment("guru.springframework.services.getProduct");
+        counterService.increment("guru.springframework.services.ProductService.getProduct");
         return productRepository.findOne(id);
     }
 
     @Override
     public List<Product> listProducts() {
         jmsTextMessageService.sendTextMessage("Listing Products");
+
+        counterService.increment("guru.springframework.services.ProductService.listProducts");
+        gaugeService.submit("guru.springframework.services.ProductService.listProducts.pageViewsPerMin", 6);
+
         return IteratorUtils.toList(productRepository.findAll().iterator());
     }
 
